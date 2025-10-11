1404df5d54fb6f80769fe50bb31103e77bd47f8f
Eldar Boltachev <135591616+eboltachev@users.noreply.github.com>
2025-10-11 20:22:17 +0300

    Perform analysis when sessions are created

---
+++ src/auto_summarization/entrypoints/schemas/session.py
@@ -0,0 +1,65 @@
+from typing import List, Optional
+
+from auto_summarization.domain.enums import StatusType
+from pydantic import BaseModel
+
+
+class SessionInfo(BaseModel):
+    session_id: str
+    title: str
+    category: str
+    text: str
+    summary: str
+    analysis: str
+    version: int
+    inserted_at: float
+    updated_at: float
+    error: Optional[str]
+
+
+class FetchSessionResponse(BaseModel):
+    sessions: List[SessionInfo]
+
+
+class CreateSessionRequest(BaseModel):
+    text: str
+    category: int
+    choices: List[int]
+    temporary: Optional[bool] = False
+
+
+class CreateSessionResponse(SessionInfo):
+    entities: str
+    sentiments: str
+    classifications: str
+    short_summary: str
+    full_summary: str
+
+
+class UpdateSessionSummarizationRequest(BaseModel):
+    session_id: str
+    summary: str
+    analysis: str
+    version: int
+
+
+class UpdateSessionSummarizationResponse(SessionInfo):
+    pass
+
+
+class UpdateSessionTitleRequest(BaseModel):
+    session_id: str
+    title: str
+    version: int
+
+
+class UpdateSessionTitleResponse(SessionInfo):
+    pass
+
+
+class DeleteSessionRequest(BaseModel):
+    session_id: str
+
+
+class DeleteSessionResponse(BaseModel):
+    status: StatusType
